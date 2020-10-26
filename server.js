eadfc46aace2b73505351017a2356c07b4d709b4
Abuhajer <Abuhagerr@gmail.com>
2020-10-26 17:41:39 +0300

    lab 7 API's

--- server.js
+++ server.js
@@ -1,61 +1,78 @@
-s'use strict';
+'use strict';
 
 // Defining Application Dependencies
 const express = require('express');
-omeeomeconst superagent = require('superagent');
+const superagent = require('superagent');
 const cors = require('cors')
 require('dotenv').config()
-
-
 const PORT = process.env.PORT || 3000;
-const GEOKEY = process.env.GEOKEY;
-const WEATHERKEY = process.env.WEATHERKEY;
-
-
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const TRAIL_API_KEY= process.env.TRAIL_API_KEY;
 const app = express();
+//--------------
 app.use(cors());
 
+//---------------
 // Routes
-app.get('/', (reqeust, response) => {
-  response.send('Home Page Welcome to express');
-});
+app.get('/weather', getWeather)
+app.get('/',welcomePage)
+app.get('/location', getLocation)
+app.get('/trails', getTrails)
 
-app.get('/location', (request, response) => {
+app.use('*', (request, resp) => {
+  resp.status(404).send('Not found!!');
+})
+//-------------------------------------
+//functions
+
+function welcomePage(request, response){
+  response.send('Home Page Welcome to express');
+}
+//----
+function getLocation(request, response){
   const city = request.query.city;
-  const url = `https://eu1.locationiq.com/v1/search.php?key=${GEOKEY}&q=${city}&format=json`;
+  const url = `https://eu1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json`;
   let location;
   superagent.get(url).then(locationData => {
     location = new Location(city, locationData.body[0]);
-    console.log(location);
     response.json(location);
-  }).catch(error => {
-    response.json(error.response)
-  }
-  )
-});
-
-
-app.get('/weather', (request, response) => {
+  }).catch(() => {
+    response.status(500).send('Sorry,something went wrong');
+  })
+}
+//----
+function getWeather(request, response) {
   const city = request.query.search_query;
   const longitude = request.query.longitude;
   const latitude = request.query.latitude;
-  const url = `https://api.weatherbit.io/v2.0/current?city=${city}&lat=${latitude}&lon=${longitude}.543&key=${WEATHERKEY}`;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&lat=${latitude}&lon=${longitude}&key=${WEATHER_API_KEY}`;
   let weather = [];
-  superagent.get(url).then(locationData => {
-    locationData.body.data.forEach(element => {
-      weather.push(new Weather(element));
+  superagent.get(url).then(weatherData => {
+    weather = weatherData.body.data.map((value) => {
+      return (new Weather(value));
     });
     response.json(weather);
-  }).catch((error) => {
-    response.json(error.response)
+  }).catch(() => {
+    response.status(500).send('Sorry,something went wrong');
   })
-
-});
-
-app.use('*', (request, resp) => {
-  resp.status(500).send('Not found');
-})
-
+}
+//----
+function getTrails(request, response){
+  const longitude = request.query.longitude;
+  const latitude = request.query.latitude;
+  const url = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&key=${TRAIL_API_KEY}`;
+  let trails = [];
+  superagent.get(url).then(trailsData => {
+    trails = trailsData.body.trails.map((value) => {
+      return (new Trail(value));
+    });
+    response.json(trails);
+  }).catch(() => {
+    response.status(500).send('Sorry,something went wrong');
+  })
+}
+//------------------------------
 // Constructor
 function Location(city, locationDatabody) {
   this.search_query = city;
@@ -67,6 +84,20 @@ function Location(city, locationDatabody) {
 function Weather(weatherData) {
   this.forecast = weatherData.weather.description;
   this.time = weatherData.datetime;
+}
+
+function Trail(trailData){
+  this.name=trailData.name;
+  this.location=trailData.location;
+  this.length=trailData.length;
+  this.stars=trailData.stars;
+  this.star_votes=trailData.starVotes;
+  this.summary=trailData.summary;
+  this.trail_url=trailData.url;
+  this.conditions=trailData.conditionStatus;
+  this.condition_date=trailData.conditionDate.split(' ')[0];
+  this.condition_time=trailData.conditionDate.split(' ')[1];
+
 }
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
