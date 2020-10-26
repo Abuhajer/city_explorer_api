87706e53ff09c2a18a38e01cd8c108a3ff7cec36
Abuhajer <Abuhagerr@gmail.com>
2020-10-26 15:45:37 +0300

    fitst api and fix lab 6

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-'use strict';
+s'use strict';
 
 // Defining Application Dependencies
 const express = require('express');
+omeeomeconst superagent = require('superagent');
 const cors = require('cors')
 require('dotenv').config()
-let error=true;
 
 
 const PORT = process.env.PORT || 3000;
+const GEOKEY = process.env.GEOKEY;
+const WEATHERKEY = process.env.WEATHERKEY;
 
 
 const app = express();
@@ -19,56 +21,47 @@ app.get('/', (reqeust, response) => {
 });
 
 app.get('/location', (request, response) => {
-  const locationData = require('./data/location.json');
   const city = request.query.city;
+  const url = `https://eu1.locationiq.com/v1/search.php?key=${GEOKEY}&q=${city}&format=json`;
   let location;
-  locationData.forEach(locationData => {
-    let display = locationData.display_name.split(',');
-    if (city === display[0].toLowerCase()) {
-      location = new Location(city, locationData);
-      error=false;
-      response.json(location);
-    }
-  });
-  if (error)
-  {
-    response.json(message());
+  superagent.get(url).then(locationData => {
+    location = new Location(city, locationData.body[0]);
+    console.log(location);
+    response.json(location);
+  }).catch(error => {
+    response.json(error.response)
   }
+  )
 });
-app.get('/weather', (request, response) => {
-  const weatherData = require('./data/weather.json');
-  const city = request.query.city;
-  let weather = [];
-  weatherData.data.forEach(locationData => {
-    if (city === weatherData['city_name'].toLowerCase()) {
-      weather.push(new Weather(locationData));
-      error=false;
-    }
 
-  });
-  if (!error)
+
+app.get('/weather', (request, response) => {
+  const city = request.query.search_query;
+  const longitude = request.query.longitude;
+  const latitude = request.query.latitude;
+  const url = `https://api.weatherbit.io/v2.0/current?city=${city}&lat=${latitude}&lon=${longitude}.543&key=${WEATHERKEY}`;
+  let weather = [];
+  superagent.get(url).then(locationData => {
+    locationData.body.data.forEach(element => {
+      weather.push(new Weather(element));
+    });
     response.json(weather);
-  else
-    response.json(message());
+  }).catch((error) => {
+    response.json(error.response)
+  })
+
 });
 
 app.use('*', (request, resp) => {
   resp.status(500).send('Not found');
 })
 
-function message() {
-  let status=500;
-  let responseText='Sorry,something went wrong'
-  let message={status:status,responseText:responseText};
-  return message;
-}
-
 // Constructor
-function Location(city, locationData) {
+function Location(city, locationDatabody) {
   this.search_query = city;
-  this.formatted_query = locationData.display_name;
-  this.latitude = locationData.lat;
-  this.longitude = locationData.lon;
+  this.formatted_query = locationDatabody.display_name;
+  this.latitude = locationDatabody.lat;
+  this.longitude = locationDatabody.lon;
 }
 
 function Weather(weatherData) {
